30915e9dbb13044aa6dbd83cd7b688790f9d5fa9
Gabriel Vander <gabsvander@hotmail.com>
2022-03-24 22:13:16 -0300

    Refactored folder structure

---
+++ app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/sdmws/core/presentation/pages/MainPage.kt
@@ -0,0 +1,9 @@
+package br.edu.ifsp.aluno.vander.gabriel.sdmws.core.presentation.pages
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
