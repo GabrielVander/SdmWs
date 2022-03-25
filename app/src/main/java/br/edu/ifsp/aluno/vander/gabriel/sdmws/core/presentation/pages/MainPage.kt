c015bcca46e47007905a93fbc31fb650525bdb86
Gabriel Vander <gabsvander@hotmail.com>
2022-03-25 20:24:08 -0300

    Implemented semesters dropdown

--- app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/sdmws/core/presentation/pages/MainPage.kt
+++ app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/sdmws/core/presentation/pages/MainPage.kt
@@ -1,13 +1,17 @@
 package br.edu.ifsp.aluno.vander.gabriel.sdmws.core.presentation.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import br.edu.ifsp.aluno.vander.gabriel.sdmws.core.domain.entities.Course
 import br.edu.ifsp.aluno.vander.gabriel.sdmws.core.presentation.manager.MainViewModel
 import br.edu.ifsp.aluno.vander.gabriel.sdmws.core.presentation.widgets.CourseDisplay
+import br.edu.ifsp.aluno.vander.gabriel.sdmws.core.presentation.widgets.SemesterDropDown
 
 @Composable
 fun MainPage(mainViewModel: MainViewModel = viewModel()) {
@@ -15,10 +19,14 @@ fun MainPage(mainViewModel: MainViewModel = viewModel()) {
 
     mainViewModel.fetchCourse()
 
-    if (course == null) {
-        Text(text = "Loading...")
-    } else {
-        CourseDisplay(course!!)
+    Scaffold {
+        if (course == null) {
+            Text(text = "Loading...")
+        } else {
+            Column(modifier = Modifier) {
+                CourseDisplay(course!!)
+                SemesterDropDown(course!!.amountOfSemesters)
+            }
+        }
     }
-
 }
---
+++ app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/sdmws/core/presentation/widgets/SemesterDropDown.kt
@@ -0,0 +1,36 @@
+package br.edu.ifsp.aluno.vander.gabriel.sdmws.core.presentation.widgets
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+
+@Composable
+fun SemesterDropDown(
+    amountOfSemesters: Int
+) {
+    var expanded: Boolean by remember { mutableStateOf(false) }
+    var chosenSemester: String? by remember { mutableStateOf(null) }
+    val semesters: List<Int> = (1..amountOfSemesters).toList()
+
+    Button(onClick = { expanded = !expanded }) {
+        Text(chosenSemester ?: "Semesters")
+        Icon(
+            imageVector = Icons.Filled.ArrowDropDown,
+            contentDescription = null,
+        )
+    }
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+    ) {
+        semesters.forEach { semesterNumber ->
+            DropdownMenuItem(onClick = {
+                expanded = false
+                chosenSemester = semesterNumber.toString()
+            }) {
+                Text(text = semesterNumber.toString())
+            }
+        }
+    }
+}
