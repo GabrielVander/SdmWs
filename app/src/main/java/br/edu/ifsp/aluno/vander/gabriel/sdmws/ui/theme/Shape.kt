6c2ed50bf62136af548c8d6e2994717d28871165
Gabriel Vander <gabsvander@hotmail.com>
2022-03-24 22:09:22 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/sdmws/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.aluno.vander.gabriel.sdmws.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
